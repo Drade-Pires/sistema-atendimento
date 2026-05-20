bb836425b1c0efdd85002190c359bab48cb5922e
Jhonatan <jhonatandeandradepires@gmail.com>
2026-05-20 15:39:13 -0300

    att bd.j

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,11 +2,14 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.DB_HOST=dpg-d86t0n0g4nts73bao540-a,
+  port: process.env.DB_PORT=5432,
+  user: process.env.DB_USER=banco_sistema_atendimento_user,
+  password: process.env.DB_PASSWORD=jbN1YwuZfWsM5WhWM9NWhvOoxrj66UgJ,
+  database: process.env.DB_NAME=banco_sistema_atendimento,
+  ssl: {
+    rejectUnauthorized: false // necessário no Render
+  }
 });
 
 module.exports = pool;
