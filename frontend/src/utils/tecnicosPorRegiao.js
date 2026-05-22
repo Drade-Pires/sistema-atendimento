c7486baaef38d4daa7f675ac814862e6570fa2fe
Jhonatan <jhonatandeandradepires@gmail.com>
2026-05-21 23:07:52 -0300

    correção 6

--- frontend/src/utils/tecnicosPorRegiao.js
+++ frontend/src/utils/tecnicosPorRegiao.js
@@ -9,9 +9,32 @@ const redIcon = new L.Icon({
   shadowSize: [41, 41]
 });
 
-const blueIcon = new L.Icon({ /* ... igual ao seu */ });
-const greenIcon = new L.Icon({ /* ... */ });
-const orangeIcon = new L.Icon({ /* ... */ });
+const blueIcon = new L.Icon({
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png",
+  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+const greenIcon = new L.Icon({
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png",
+  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+const orangeIcon = new L.Icon({
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-orange.png",
+  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
 
 export const tecnicosPorRegiao = {
   "São Paulo": {
